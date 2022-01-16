c95fb3f617476b0678a401e43f78b61deb496407
ricardo <thespirit@vip.qq.com>
2022-01-17 01:56:24 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+
+namespace AntlrLearn{
+    public class Program{
+        public static void Main(string[] args){
+            StringParserListener("var i = 0;if(i == 0)Console.WriteLine('hello world');");
+        }
+
+        public static void StringParserListener(string text){
+            // var charStream = CharStreams.fromString(text);
+            // var lexer = new ruleLexer(charStream);
+            // var stream = new CommonTokenStream(lexer);
+            // var parser = new ruleParser(stream);
+            // parser.BuildParseTree = true;
+            // var tree = parser.program();
+            // var extractor = new ruleBaseListener();
+            // ParseTreeWalker.Default.Walk(extractor, tree);
+        }
+    }
+}
